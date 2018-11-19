fc289d159afbc494b8a3ced0424e1c92f492719c
jmwatson <jake.watson76@gmail.com>
2018-11-19 14:17:16 -0600

    Implementing the ability to add items of differing values to the total.

--- src/CheckoutOrder.py
+++ src/CheckoutOrder.py
@@ -1,6 +1,10 @@
 class CheckoutOrder:
     def __init__(self):
         self.__value = 0.0
+        self.__items = {
+            'item': 1.00,
+            'second_item': 1.50,
+        }
         return
 
     def add_item(self, item, value):
@@ -8,5 +12,5 @@ class CheckoutOrder:
 
 
     def scan_item(self, item):
-        self.__value = self.__value + 1.00
+        self.__value = self.__value + self.__items[item]
         return self.__value
