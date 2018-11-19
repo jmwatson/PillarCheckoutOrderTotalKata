c5168e0e295ec15a3be13e43051b775d023df0b6
jmwatson <jake.watson76@gmail.com>
2018-11-19 14:27:19 -0600

    Fixed logic issue with adding an item to the system.

--- src/CheckoutOrder.py
+++ src/CheckoutOrder.py
@@ -1,16 +1,13 @@
 class CheckoutOrder:
     def __init__(self):
         self.__value = 0.0
-        self.__items = {
-            'item': 1.00,
-            'second_item': 1.50,
-        }
+        self.__items = {}
         return
 
     def add_item(self, item, value):
         flag = False
 
-        if not self.__items[item]:
+        if item not in self.__items:
             flag = True
 
         return flag
