407e5147c1bb2273bfcc8ffd0ed967c19200c907
jmwatson <jake.watson76@gmail.com>
2018-11-19 14:22:21 -0600

    Refactored out the setup and destruction of the checkout_order instance used by all tests.

--- test/CheckoutOrderTests.py
+++ test/CheckoutOrderTests.py
@@ -3,17 +3,23 @@ from src.CheckoutOrder import CheckoutOrder
 
 
 class CheckoutOrderTests(unittest.TestCase):
+    def setUp(self):
+        self.checkout_order = CheckoutOrder()
+
+
+    def tearDown(self):
+        self.checkout_order = None
+
+
     def test_add_item(self):
-        checkout_order = CheckoutOrder()
-        self.assertTrue(checkout_order.add_item('item', 1))
+        self.assertTrue(self.checkout_order.add_item('item', 1))
 
 
     def test_scan_item(self):
-        checkout_order = CheckoutOrder()
-        self.assertEqual(1.00, checkout_order.scan_item('item'))
-        self.assertEqual(2.00, checkout_order.scan_item('item'))
-        self.assertEqual(3.50, checkout_order.scan_item('second_item'))
-        self.assertEqual(5.00, checkout_order.scan_item('second_item'))
+        self.assertEqual(1.00, self.checkout_order.scan_item('item'))
+        self.assertEqual(2.00, self.checkout_order.scan_item('item'))
+        self.assertEqual(3.50, self.checkout_order.scan_item('second_item'))
+        self.assertEqual(5.00, self.checkout_order.scan_item('second_item'))
 
 
 if __name__ == 'main':
