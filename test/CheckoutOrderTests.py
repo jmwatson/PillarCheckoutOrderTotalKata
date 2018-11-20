04c930451aa339072baebaaf94bcbee4ab35c45e
jmwatson <jake.watson76@gmail.com>
2018-11-20 13:51:42 -0600

    Fixed incorrect test value.

--- test/CheckoutOrderTests.py
+++ test/CheckoutOrderTests.py
@@ -101,7 +101,7 @@ class CheckoutOrderTests(unittest.TestCase):
         self.assertEqual(5.00, self.checkout_order.scan_item('bundle_item'))
         self.assertEqual(6.00, self.checkout_order.scan_item('bundle_item'))
         self.assertEqual(8.20, self.checkout_order.scan_item_by_weight('bundle_item', 1.1))
-        self.assertEqual(8.20, self.checkout_order.scan_item_by_weight('bundle_item', 1.1))
+        self.assertEqual(9.00, self.checkout_order.scan_item_by_weight('bundle_item', 1.1))
 
 
 if __name__ == 'main':
